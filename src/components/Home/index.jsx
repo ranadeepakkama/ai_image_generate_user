3bff1cba15bdfb14ec7ada7362ce292c6c842183
ranadeepakkama <ranadeepakbunny35@gmail.com>
2025-07-18 18:42:15 +0530

    updated code

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,27 @@
 import Navbar from "../Navbar/navbar"
 import './style.css'
 import ImageUploadAndConvert from '../Upload&ConvertImage/index'
+import { v4 as uuidv4 } from 'uuid';
+const id = uuidv4();
 
 const Home = () => {
+    const examplesList = ["A close-up photo of coffee beans","A photo of a forest canopy with blue skies from below","a polaroid portrait of a dog wearing sunglasses","a duffle bag made of cheese"]
+
+
+
     return(
         <div className="main-component">
             <Navbar/>
             <div className="image-component">
                 <ImageUploadAndConvert/>
+            </div>
+            <div className="example-prompt-container">
+                <p className="example-header">U can try Examples Prompts Like:</p>
+                <ul>
+                    {examplesList.map(prompt => (
+                        <li key={id}>{prompt}</li>
+                    ))}
+                </ul>
             </div>
             
         </div>
